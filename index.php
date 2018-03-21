64c626168d94d722a0de2e9c72a6a9381dc39ab4
cassandrehenriquez <chenriq@fau.edu>
2018-03-21 08:37:51 -0400

    checking database for duplicated emails

--- index.php
+++ index.php
@@ -7,13 +7,21 @@ if($db === false){
 }
 $userEmail = $_POST['email'];
 $timestamp = date('Y-m-d G:i:s');
+$message = '';
 
+$select ="SELECT * FROM signinform WHERE email = '$userEmail'";
+$result = mysqli_query($db, $select);
+
+if(mysqli_fetch_assoc($result)) {
+    return true;
+} else {
+    $message = 'This email is already being used';
+}
 
 if(isset($_POST['submit'])) {
     $sql = "INSERT INTO signinform (email, created_on) VALUES('$userEmail', '$timestamp')";
     if(mysqli_query($db, $sql)){
         $message = "Thanks for signing up! You'll recieve an email with your invititation";
-        echo "Records added successfully.";
     } else{
         $message = "There was a problem with your request: valid email address not provided";
     }
